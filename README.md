0c29a96c7ac460bfa4e58925e080a2a184f4d79d
Aadhya Puttur <putturaadhya@gmail.com>
2026-01-14 13:16:12 -0800

    Clean portfolio initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aadhya_personal_website
+I am creating a website inspired by other personal websites I am using their creativity in order to learn about building a website!
---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+const myBirthday = '18.04.1982'; //declaring this variable cannot be changed
+const COLOR_RED = "naming convention";
+const pageLoadTime = 3; //calculated during run time
+
+let x = "10" + 10; //cant do this
+
+let y = "10" + "10"; //cant do this
+
+let z = "10" - "10";//can do this and all other operations
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log("Hello, World!")
